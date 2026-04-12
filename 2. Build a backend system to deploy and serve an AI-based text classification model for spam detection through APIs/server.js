eaee8f1a9dad2f18ccf28fc78bf4e2b30d6877f1
tushar-git-10 <tushar.kokare.aids.2024@vpkbiet.org>
2026-04-12 12:31:11 +0530

    Update server.js

--- 2. Build a backend system to deploy and serve an AI-based text classification model for spam detection through APIs/server.js
+++ 2. Build a backend system to deploy and serve an AI-based text classification model for spam detection through APIs/server.js
@@ -1,46 +1,27 @@
 const express = require("express");
 const app = express();
 
-// list of spam words
 const spamWords = ["win", "free", "offer", "money", "prize"];
 
-// API
 app.get("/predict", (req, res) => {
 
-  // get text from URL (default = "win")
-  let text = req.query.text;
+  const text = (req.query.text || "").toLowerCase();
 
-  text = text.toLowerCase();
-
-  // check spam manually
   let spam = false;
 
-  for (let i = 0; i < spamWords.length; i++) {
-    if (text.includes(spamWords[i])) {
+  for (let word of spamWords) {
+    if (text.includes(word)) {
       spam = true;
+      break;
     }
   }
 
-  let result;
   if (spam) {
-    result = "Spam 🚫";
+    res.json({ result: "Spam 🚫" });
   } else {
-    result = "Not Spam ✅";
+    res.json({ result: "Not Spam ✅" });
   }
 
-  // send response
-  res.json({
-    input: text,
-    result: result
-  });
-
 });
 
-// start server
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
-});
-
-
-
-
+app.listen(3000, () => console.log("Server running 🚀"));
